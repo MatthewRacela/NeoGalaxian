6605aff1cc3b46a57a5e3336c550becbb7837a0b
MUKILTEO\1707226 <1707226@mukilteo.wednet.edu>
2021-06-07 10:03:26 -0700

    Lovely day for progress

---
+++ Neo Galaxian/Assets/Pause.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("Fire2"))
+        {
+            
+        }
+    }
+
+    
+}
